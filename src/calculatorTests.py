7885623dd7a4b679061965e903b6ce235950d846
rutvik2611 <rutvik2611@gmail.com>
2019-10-19 00:08:32 -0300

    Feature:8 unit test were added

---
+++ src/calculatorTests.py
@@ -0,0 +1,44 @@
+import unittest
+from calculator import calculator
+
+
+class MyTestCase(unittest.TestCase):
+    def setUp(self) -> None:
+        self.calculator = calculator()
+
+    def test_instantiate_calculator(self):
+        self.assertIsInstance(self.calculator, calculator)
+
+    def test_addition(self):
+        self.calculator.__add__(1, 1)
+        self.assertEqual(self.calculator.result, 2)
+
+
+    def test_subtraction(self):
+        self.calculator.__sub__(5, 5)
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_multiplication(self):
+        self.calculator.__mul__(5, 5)
+        self.assertEqual(self.calculator.result, 25)
+
+    def test_division(self):
+        self.calculator.__div__(5, 5)
+        self.assertEqual(self.calculator.result, 1)
+
+    def test_square(self):
+        self.calculator.__square__(5)
+        self.assertEqual(self.calculator.result, 25)
+
+    def test_squareRoot(self):
+        self.calculator.__squareRoot__(25)
+        self.assertEqual(self.calculator.result, 5)
+
+
+    def test_results_property(self):
+        self.assertEqual(self.calculator.result, 0)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
