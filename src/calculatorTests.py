ac15594bd92de757884430ed8bad59258a64996c
rutvik2611 <rutvik2611@gmail.com>
2019-11-01 00:53:23 -0300

    Feature:Added Unit test of Mul,Div,Square and SquareRoot

--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -33,70 +33,54 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.result, c[var]*-1)
 
 
-    # def test_multiplication(self):
-    #
-    #         x = self.calculator.read_csv("../src/CSV/Unit Test Multiplication.csv")
-    #
-    #         a = []
-    #         b = []
-    #         c = []
-    #         for var in range(len(x)):
-    #             a.append(x[var][0])
-    #             b.append(x[var][1])
-    #             c.append(x[var][2])
-    #             self.calculator.__mul__(a[var], b[var])
-    #             self.assertEqual(self.calculator.result, c[var])
-    #
-    #
-    #
-    # def test_division(self):
-    #     x = self.calculator.read_csv("../src/CSV/Unit Test Division.csv")
-    #
-    #     a = []
-    #     b = []
-    #     c = []
-    #     for var in range(len(x)):
-    #         a.append(int(x[var][0]))
-    #         b.append(int(x[var][1]))
-    #         c.append(int(x[var][2]))
-    #         self.calculator.__div__(b[var],a[var])
-    #         self.assertEqual(int(self.calculator.result), c[var])
-    #
-    # def test_square(self):
-    #
-    #     x = self.calculator.read_csv("../src/CSV/Unit Test Square.csv")
-    #
-    #     a = []
-    #     b = []
-    #
-    #     for var in range(len(x)):
-    #         a.append(int(x[var][0]))
-    #         b.append(int(x[var][1]))
-    #
-    #         self.calculator.__square__(a[var])
-    #         self.assertEqual(int(self.calculator.result), b[var])
-    #
-    #
-    # def test_squareRoot(self):
-    #     x = self.calculator.read_csv("../src/CSV/Unit Test Square Root.csv")
-    #
-    #     a = []
-    #     b = []
-    #
-    #     for var in range(len(x)):
-    #         a.append(int(x[var][0]))
-    #         b.append(int(x[var][1]))
-    #
-    #         self.calculator.__squareRoot__(a[var])
-    #         self.assertEqual(int(self.calculator.result), b[var])
-    #
-    #
-    #
-    #
-    # def test_results_property(self):
-    #
-    #     self.assertEqual(self.calculator.result, 0)
-    #
+    def test_multiplication(self):
+
+        y = self.calculator.read_csv("../src/CSV/Unit Test Multiplication.csv")
+        a = y[0]
+        b = y[1]
+        c = y[2]
+        for var in range(len(a)):
+            self.calculator.__mul__(a[var], b[var])
+            self.assertEqual(self.calculator.result, c[var])
+
+
+
+    def test_division(self):
+        y = self.calculator.read_csv("../src/CSV/Unit Test Division.csv")
+        a = y[0]
+        b = y[1]
+        c = y[2]
+        for var in range(len(a)):
+            self.calculator.__div__(b[var],a[var])
+            self.assertEqual(int(self.calculator.result),int(c[var]))
+
+    def test_square(self):
+
+        y = self.calculator.read_csv("../src/CSV/Unit Test Square.csv")
+        a = y[0]
+        b = y[1]
+
+        for var in range(len(a)):
+            self.calculator.__square__(a[var])
+            self.assertEqual(self.calculator.result,b[var])
+
+
+    def test_squareRoot(self):
+        y = self.calculator.read_csv("../src/CSV/Unit Test Square Root.csv")
+        a = y[0]
+        b = y[1]
+
+        for var in range(len(a)):
+            self.calculator.__squareRoot__(a[var])
+            self.assertEqual(int(self.calculator.result), int(b[var]))
+
+
+
+
+    def test_results_property(self):
+
+        self.assertEqual(self.calculator.result, 0)
+
 
 
 
